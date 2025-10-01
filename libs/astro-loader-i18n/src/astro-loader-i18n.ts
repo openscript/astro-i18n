47953b43e38636c4b932946dc59fe4ab836e97cb
Robin <git+r@obin.ch>
2025-10-01 07:21:49 +0000

    chore: refactor content loaders

--- libs/astro-loader-i18n/src/astro-loader-i18n.ts
+++ libs/astro-loader-i18n/src/astro-loader-i18n.ts
@@ -1,5 +1,6 @@
-export { i18nLoader } from "./loaders/i18n-loader";
 export { i18nContentLoader } from "./loaders/i18n-content-loader";
+export { i18nFileLoader } from "./loaders/i18n-file-loader";
+export { i18nLoader } from "./loaders/i18n-loader";
 export { localized } from "./schemas/i18n-content-schema";
 export { extendI18nLoaderSchema, i18nLoaderSchema } from "./schemas/i18n-loader-schema";
 export { i18nPropsAndParams, i18nProps } from "./props-and-params/i18n-props-and-params";
